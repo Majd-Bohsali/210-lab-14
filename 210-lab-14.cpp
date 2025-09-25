bc25423dce19e1643f00848d900e7d1d45a98cc9
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-24 17:29:08 -0700

    created class with getter and setter methods

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 const int W15 = 15, SIZE = 5;
 
+class Color {
+private:
+    int redVal; 
+    int greenVal;
+    int blueVal;
+public: 
+    int getRedVal()              { return redVal; }
+    void setRedVal(int r)        {redVal = r; }
+    int getGreenVal()            { return greenVal; }
+    void setGreenVal(int g)      {greenVal = g; }
+    int getBlueVal()             { return blueVal; }
+    void setBlueVal(int b)       {blueVal = b; }
+};
+
 int main() { 
     return 0;
 }
