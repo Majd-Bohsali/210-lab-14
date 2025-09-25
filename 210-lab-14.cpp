5ae0769a31adf77fc3489b972b61bb0ba50f4404
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-24 17:23:13 -0700

    Inital Commit

---
+++ 210-lab-14.cpp
@@ -0,0 +1,12 @@
+// COMSC210 | Lab 14 | Majd Bohsali
+// IDE used: Visual Studi Code
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int W15 = 15, SIZE = 5;
+
+int main() { 
+    return 0;
+}
